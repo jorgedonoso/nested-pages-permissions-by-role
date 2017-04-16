c5ba94d097d6b9e8784e126de22193a6d9f8bea3
Jorge Donoso <jdevcl@gmail.com>
2017-04-16 09:38:06 -0400

    Add column sorting to admin area.

--- helpers.php
+++ helpers.php
@@ -13,4 +13,31 @@ function _helpers_wp_get_abs_parent_id($post_id){
     return $parent_id;
 }
 
+/*
+* Updates the parent meta for a specific post.
+*/
+function update_parent_meta($id){
+    
+    $parent_id = wp_get_post_parent_id($id);
+
+    if($parent_id==0){
+        $parent_id = $id;
+    }
+
+    $the_parent_title = get_the_title($parent_id);
+    
+    update_post_meta($id, '_nppbr_parent_page', $the_parent_title);
+}
+
+/*
+* Updates the absolute parent meta for a specific post.
+*/
+function update_absolute_parent_meta($id){
+
+    $absolute_parent_id = _helpers_wp_get_abs_parent_id($id);
+    $the_absolute_title = get_the_title($absolute_parent_id);
+
+    update_post_meta($id, '_nppbr_absolute_parent_page', $the_absolute_title);
+}
+
 ?>
--- nested-pages-permissions-by-role.php
+++ nested-pages-permissions-by-role.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Nested Pages Permissions by Role
-Description: This plugin restricts users to only be able to edit pages under their assigned page tree.
+Description: This plugin restricts users from editing pages outside their assigned page tree.
 Version: 0.2.25
 Author: Jorge Donoso
 */
@@ -30,27 +30,83 @@ function nested_pages_permissions_by_role_column_content($column_name, $id){
 	* Populate direct parent content.
 	*/
     if($column_name === 'nested_pages_permissions_by_role_parent_page'){
-        $parent_id = wp_get_post_parent_id($id);
-        
-        if($parent_id!=0){
-            $the_title = get_the_title($parent_id);
-        }
-
-        echo $the_title;
+        echo get_post_meta($id, '_nppbr_parent_page', true );
     }
 
     /*
     * Populate absolute parent content.
     */
     if($column_name === 'nested_pages_permissions_by_role_absolute_parent_page'){
-       	$absolute_parent_id = _helpers_wp_get_abs_parent_id($id);
-
-       	if($id != $absolute_parent_id){
-        	$the_title = get_the_title($absolute_parent_id);
-    	}
-
-        echo $the_title;
+       	echo get_post_meta($id, '_nppbr_absolute_parent_page', true );
     }
 }
 
+/*
+* Make columns sortable
+*/
+add_filter('manage_edit-page_sortable_columns','nested_pages_permissions_by_role_sortable_columns');
+
+function nested_pages_permissions_by_role_sortable_columns($columns){
+	$columns['nested_pages_permissions_by_role_parent_page'] = 'parent_page';
+	$columns['nested_pages_permissions_by_role_absolute_parent_page'] = 'absolute_parent_page';
+	return $columns;
+}
+
+add_action('pre_get_posts', 'nppbr_orderby');
+function nppbr_orderby($query) {
+
+	if($query->get("post_type")=='page'){
+
+		$orderby = $query->get('orderby');
+		
+		if($orderby == 'parent_page'){
+            $query->set('meta_key','_nppbr_parent_page');
+            $query->set('orderby','meta_value');
+		}
+		
+		if($orderby == 'absolute_parent_page'){
+            $query->set('meta_key','_nppbr_absolute_parent_page');
+            $query->set('orderby','meta_value');
+		}
+	}
+
+	return $query;
+}
+
+/*
+* Activation hook.
+*/
+register_activation_hook( __FILE__, 'nppbr_activation' );
+function nppbr_activation() {
+	
+	$pages = get_pages(); 
+	foreach ( $pages as $page ) {
+		update_parent_meta($page->ID);
+		update_absolute_parent_meta($page->ID);
+ 	}
+
+}
+
+/*
+* Deactivation hook.
+*/
+register_deactivation_hook( __FILE__, 'nppbr_deactivation' );
+
+function nppbr_deactivation(){
+	$pages = get_pages(); 
+	foreach ( $pages as $page ) {
+		delete_post_meta($page->ID,'_nppbr_parent_page');
+		delete_post_meta($page->ID,'_nppbr_absolute_parent_page');
+ 	}
+}
+
+/*
+* Update the meta on create or update.
+*/
+add_action('save_post_page', 'nppbr_save_post_page' );
+
+function nppbr_save_post_page($id){
+	update_parent_meta($id);
+	update_absolute_parent_meta($id);
+}
 ?>
